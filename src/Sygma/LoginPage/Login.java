b6a8e536d1119d67a3e40aadf07a30fb4d3e5321
Vea <Vea@DESKTOP-23A0JSK>
2024-09-01 16:47:46 +0800

    new

--- src/Sygma/LoginPage/Login.java
+++ src/Sygma/LoginPage/Login.java
@@ -42,7 +42,7 @@ public class Login extends javax.swing.JFrame {
     
         showForm(Log);
          idtext();
-        gate.setVisible(false);
+        JLabel7.setVisible(true);
         initMoving(this);
     }
      private int x;
@@ -52,18 +52,18 @@ public class Login extends javax.swing.JFrame {
          Log.loging.removeAll();
          Log.loging.repaint();
          Log.loging.revalidate();
-         Log.loging.setText(gate.getText());         
+         Log.loging.setText(JLabel7.getText());         
         });
          SwingUtilities.invokeLater(() -> {
          SignUp.genId.removeAll();
          SignUp.genId.repaint();
          SignUp.genId.revalidate();
-         SignUp.genId.setText(gate.getText());         
+         SignUp.genId.setText(JLabel7.getText());         
         }); 
        }
        
          public void initMoving(JFrame frame){
-        changeform.addMouseListener(new MouseAdapter() {
+        jPanel1.addMouseListener(new MouseAdapter() {
             @Override
             public void mousePressed(MouseEvent e) {
                 
@@ -72,7 +72,7 @@ public class Login extends javax.swing.JFrame {
             }
             
         });
-        changeform.addMouseMotionListener(new MouseMotionAdapter() {
+        jPanel1.addMouseMotionListener(new MouseMotionAdapter() {
             @Override
             public void mouseDragged(MouseEvent e) {
              frame.setLocation(e.getXOnScreen()-x, e.getYOnScreen()-y);
@@ -101,54 +101,68 @@ public class Login extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        gate = new javax.swing.JLabel();
+        jPanel1 = new javax.swing.JPanel();
         jLabel4 = new javax.swing.JLabel();
-        jLabel5 = new javax.swing.JLabel();
         jLabel6 = new javax.swing.JLabel();
+        jLabel5 = new javax.swing.JLabel();
+        JLabel7 = new javax.swing.JLabel();
         jLabel2 = new javax.swing.JLabel();
         changeform = new javax.swing.JPanel();
+        gate = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setUndecorated(true);
         getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
-        gate.setIcon(new javax.swing.ImageIcon(getClass().getResource("/icon/picwall2.png"))); // NOI18N
-        gate.setText("jLabel3");
-        getContentPane().add(gate, new org.netbeans.lib.awtextra.AbsoluteConstraints(60, 60, 250, 250));
+        jPanel1.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
         jLabel4.setFont(new java.awt.Font("Segoe UI", 1, 36)); // NOI18N
         jLabel4.setForeground(new java.awt.Color(255, 255, 255));
         jLabel4.setText("SYGMA");
-        getContentPane().add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(120, 310, 130, 40));
-
-        jLabel5.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
-        jLabel5.setForeground(new java.awt.Color(255, 255, 255));
-        jLabel5.setText("The Smart Expense Tracker");
-        getContentPane().add(jLabel5, new org.netbeans.lib.awtextra.AbsoluteConstraints(80, 350, 240, 30));
+        jPanel1.add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(120, 310, 130, 40));
 
         jLabel6.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
         jLabel6.setForeground(new java.awt.Color(255, 255, 255));
         jLabel6.setText("Made by TEAM");
-        getContentPane().add(jLabel6, new org.netbeans.lib.awtextra.AbsoluteConstraints(140, 380, 120, 30));
+        jPanel1.add(jLabel6, new org.netbeans.lib.awtextra.AbsoluteConstraints(140, 380, 120, 30));
+
+        jLabel5.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
+        jLabel5.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel5.setText("The Smart Expense Tracker");
+        jPanel1.add(jLabel5, new org.netbeans.lib.awtextra.AbsoluteConstraints(80, 350, 240, 30));
+
+        JLabel7.setIcon(new javax.swing.ImageIcon(getClass().getResource("/icon/picwall2.png"))); // NOI18N
+        JLabel7.setText("jLabel3");
+        jPanel1.add(JLabel7, new org.netbeans.lib.awtextra.AbsoluteConstraints(60, 60, 250, 250));
 
         jLabel2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/icon/w22.png"))); // NOI18N
         jLabel2.setText("jLabel2");
-        getContentPane().add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 380, 460));
+        jPanel1.add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 380, 460));
 
         changeform.setBackground(new java.awt.Color(255, 255, 255));
+
+        gate.setText("jLabel1");
 
         javax.swing.GroupLayout changeformLayout = new javax.swing.GroupLayout(changeform);
         changeform.setLayout(changeformLayout);
         changeformLayout.setHorizontalGroup(
             changeformLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 380, Short.MAX_VALUE)
+            .addGroup(changeformLayout.createSequentialGroup()
+                .addGap(27, 27, 27)
+                .addComponent(gate)
+                .addContainerGap(316, Short.MAX_VALUE))
         );
         changeformLayout.setVerticalGroup(
             changeformLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 460, Short.MAX_VALUE)
+            .addGroup(changeformLayout.createSequentialGroup()
+                .addGap(21, 21, 21)
+                .addComponent(gate)
+                .addContainerGap(423, Short.MAX_VALUE))
         );
 
-        getContentPane().add(changeform, new org.netbeans.lib.awtextra.AbsoluteConstraints(380, 0, 380, 460));
+        jPanel1.add(changeform, new org.netbeans.lib.awtextra.AbsoluteConstraints(380, 0, 380, 460));
+
+        getContentPane().add(jPanel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, -1, -1));
 
         pack();
         setLocationRelativeTo(null);
@@ -190,11 +204,13 @@ public class Login extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel JLabel7;
     private javax.swing.JPanel changeform;
     public javax.swing.JLabel gate;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel4;
     private javax.swing.JLabel jLabel5;
     private javax.swing.JLabel jLabel6;
+    private javax.swing.JPanel jPanel1;
     // End of variables declaration//GEN-END:variables
 }
