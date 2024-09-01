a39e73094150aceaf39ba5ead8f4cf4d7147c86a
Vea <Vea@DESKTOP-23A0JSK>
2024-09-01 18:23:45 +0800

    version1

--- src/Sygma/LoginPage/Login.java
+++ src/Sygma/LoginPage/Login.java
@@ -36,10 +36,11 @@ public class Login extends javax.swing.JFrame {
         jLabel6 = new javax.swing.JLabel();
         jLabel5 = new javax.swing.JLabel();
         JLabel7 = new javax.swing.JLabel();
-        jLabel2 = new javax.swing.JLabel();
-        changeform = new javax.swing.JPanel();
         gate = new javax.swing.JLabel();
-        jButton1 = new javax.swing.JButton();
+        jLabel2 = new javax.swing.JLabel();
+        jPanel2 = new javax.swing.JPanel();
+        jButton9 = new javax.swing.JButton();
+        changeform = new javax.swing.JPanel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setUndecorated(true);
@@ -66,44 +67,67 @@ public class Login extends javax.swing.JFrame {
         JLabel7.setText("jLabel3");
         jPanel1.add(JLabel7, new org.netbeans.lib.awtextra.AbsoluteConstraints(60, 60, 250, 250));
 
+        gate.setText("jLabel1");
+        jPanel1.add(gate, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, -1, -1));
+
         jLabel2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/icon/w22.png"))); // NOI18N
         jLabel2.setText("jLabel2");
         jPanel1.add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 380, 460));
 
+        jPanel2.setBackground(new java.awt.Color(255, 255, 255));
+
+        jButton9.setBackground(new java.awt.Color(253, 253, 253));
+        jButton9.setFont(new java.awt.Font("Segoe UI", 1, 16)); // NOI18N
+        jButton9.setForeground(new java.awt.Color(255, 255, 255));
+        jButton9.setIcon(new javax.swing.ImageIcon(getClass().getResource("/icon/close.png"))); // NOI18N
+        jButton9.setBorder(null);
+        jButton9.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton9ActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
+        jPanel2.setLayout(jPanel2Layout);
+        jPanel2Layout.setHorizontalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel2Layout.createSequentialGroup()
+                .addGap(0, 352, Short.MAX_VALUE)
+                .addComponent(jButton9))
+        );
+        jPanel2Layout.setVerticalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel2Layout.createSequentialGroup()
+                .addComponent(jButton9)
+                .addGap(0, 13, Short.MAX_VALUE))
+        );
+
+        jPanel1.add(jPanel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(380, 0, 380, 40));
+
         changeform.setBackground(new java.awt.Color(255, 255, 255));
-
-        gate.setText("jLabel1");
-
-        jButton1.setText("jButton1");
 
         javax.swing.GroupLayout changeformLayout = new javax.swing.GroupLayout(changeform);
         changeform.setLayout(changeformLayout);
         changeformLayout.setHorizontalGroup(
             changeformLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(changeformLayout.createSequentialGroup()
-                .addGap(27, 27, 27)
-                .addComponent(gate)
-                .addGap(28, 28, 28)
-                .addComponent(jButton1)
-                .addContainerGap(213, Short.MAX_VALUE))
+            .addGap(0, 380, Short.MAX_VALUE)
         );
         changeformLayout.setVerticalGroup(
             changeformLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(changeformLayout.createSequentialGroup()
-                .addGap(18, 18, 18)
-                .addGroup(changeformLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(gate)
-                    .addComponent(jButton1))
-                .addContainerGap(419, Short.MAX_VALUE))
+            .addGap(0, 420, Short.MAX_VALUE)
         );
 
-        jPanel1.add(changeform, new org.netbeans.lib.awtextra.AbsoluteConstraints(380, 0, 380, 460));
+        jPanel1.add(changeform, new org.netbeans.lib.awtextra.AbsoluteConstraints(380, 40, 380, 420));
 
         getContentPane().add(jPanel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, -1, -1));
 
         pack();
         setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jButton9ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton9ActionPerformed
+        System.exit(0);
+    }//GEN-LAST:event_jButton9ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -144,11 +168,12 @@ public class Login extends javax.swing.JFrame {
     private javax.swing.JLabel JLabel7;
     private javax.swing.JPanel changeform;
     public javax.swing.JLabel gate;
-    private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton9;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel4;
     private javax.swing.JLabel jLabel5;
     private javax.swing.JLabel jLabel6;
     private javax.swing.JPanel jPanel1;
+    private javax.swing.JPanel jPanel2;
     // End of variables declaration//GEN-END:variables
 }
--- src/Sygma/LoginPage/log.java
+++ src/Sygma/LoginPage/log.java
@@ -4,6 +4,32 @@
  */
 package Sygma.LoginPage;
 
+import java.util.Arrays;
+import java.util.prefs.Preferences;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
+import Sygma.Controller.userController;
+import Sygma.Model.ModelUser;
+import Sygma.main; 
+
+
+import com.formdev.flatlaf.FlatLightLaf;
+import java.awt.Color;
+import java.awt.Cursor;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionAdapter;
+import java.util.Arrays;
+import java.util.Random;
+import java.util.UUID;
+import java.util.prefs.Preferences;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
 
 /**
  *
@@ -16,7 +42,37 @@ public class log extends javax.swing.JPanel {
      */
     public log() {
         initComponents();
-      
+        loging.setVisible(false);
+          username.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyPressed(KeyEvent e) {
+                if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+                    jButton2ActionPerformed(null);
+                }
+            }
+        });
+
+        password.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyPressed(KeyEvent e) {
+                if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+                    jButton2ActionPerformed(null);
+                }
+            }
+        });
+        
+    
+    }
+    
+      private static String generateUID(){
+        int userId = 6;
+        Random random = new Random();
+        StringBuilder userBuilder = new StringBuilder();
+        for (int i = 0; i < userId; i++) {
+            userBuilder.append(random.nextInt(10));
+        }
+        String uID = userBuilder.toString();
+        return uID;
     }
 
     /**
@@ -29,59 +85,155 @@ public class log extends javax.swing.JPanel {
     private void initComponents() {
 
         loging = new javax.swing.JLabel();
-        label = new javax.swing.JLabel();
-        jButton1 = new javax.swing.JButton();
+        p2 = new Sygma.Components.PanelBar();
+        password = new javax.swing.JPasswordField();
+        p1 = new Sygma.Components.PanelBar();
+        username = new javax.swing.JTextField();
+        panelBar3 = new Sygma.Components.PanelBar();
+        jButton3 = new javax.swing.JButton();
+        jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        jButton2 = new javax.swing.JButton();
 
         setBackground(new java.awt.Color(255, 255, 255));
+        setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
-        loging.setText("jLabel1");
+        loging.setFont(new java.awt.Font("Segoe UI", 0, 8)); // NOI18N
+        loging.setText("lable");
+        add(loging, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 10, 60, 30));
 
-        label.setText("jLabel1");
+        p2.setBackground(new java.awt.Color(255, 255, 255));
+        p2.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
-        jButton1.setText("jButton1");
-        jButton1.addActionListener(new java.awt.event.ActionListener() {
+        password.setForeground(new java.awt.Color(153, 153, 153));
+        password.setBorder(javax.swing.BorderFactory.createEmptyBorder(1, 1, 1, 1));
+        p2.add(password, new org.netbeans.lib.awtextra.AbsoluteConstraints(6, 6, 198, 20));
+
+        add(p2, new org.netbeans.lib.awtextra.AbsoluteConstraints(80, 220, 210, 30));
+
+        p1.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        username.setBackground(new java.awt.Color(242, 242, 242));
+        username.setForeground(new java.awt.Color(153, 153, 153));
+        username.setBorder(javax.swing.BorderFactory.createEmptyBorder(1, 1, 1, 1));
+        p1.add(username, new org.netbeans.lib.awtextra.AbsoluteConstraints(6, 6, 198, -1));
+
+        add(p1, new org.netbeans.lib.awtextra.AbsoluteConstraints(80, 180, 210, 30));
+
+        panelBar3.setBackground(new java.awt.Color(110, 206, 209));
+
+        jButton3.setBackground(new java.awt.Color(110, 206, 209));
+        jButton3.setFont(new java.awt.Font("Segoe UI", 0, 15)); // NOI18N
+        jButton3.setForeground(new java.awt.Color(255, 255, 255));
+        jButton3.setText("Sign-up");
+        jButton3.setBorder(null);
+        jButton3.setHorizontalAlignment(javax.swing.SwingConstants.LEFT);
+        jButton3.setIconTextGap(5);
+        jButton3.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton1ActionPerformed(evt);
+                jButton3ActionPerformed(evt);
             }
         });
 
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
-        this.setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(171, 171, 171)
-                        .addComponent(loging))
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(68, 68, 68)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 151, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(label))))
-                .addContainerGap(48, Short.MAX_VALUE))
+        javax.swing.GroupLayout panelBar3Layout = new javax.swing.GroupLayout(panelBar3);
+        panelBar3.setLayout(panelBar3Layout);
+        panelBar3Layout.setHorizontalGroup(
+            panelBar3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(panelBar3Layout.createSequentialGroup()
+                .addGap(33, 33, 33)
+                .addComponent(jButton3)
+                .addContainerGap(35, Short.MAX_VALUE))
         );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap(128, Short.MAX_VALUE)
-                .addComponent(loging)
-                .addGap(29, 29, 29)
-                .addComponent(label)
-                .addGap(45, 45, 45)
-                .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 71, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(21, 21, 21))
+        panelBar3Layout.setVerticalGroup(
+            panelBar3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(panelBar3Layout.createSequentialGroup()
+                .addComponent(jButton3)
+                .addGap(0, 9, Short.MAX_VALUE))
         );
+
+        add(panelBar3, new org.netbeans.lib.awtextra.AbsoluteConstraints(130, 280, 120, 30));
+
+        jLabel1.setForeground(new java.awt.Color(172, 172, 172));
+        jLabel1.setText("You don't have an account?");
+        add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(110, 310, 160, 20));
+
+        jLabel2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Sygma/LoginPage/icons8-sensor-50.png"))); // NOI18N
+        add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(160, 60, 50, 60));
+
+        jButton2.setBackground(new java.awt.Color(253, 253, 253));
+        jButton2.setFont(new java.awt.Font("Segoe UI", 1, 32)); // NOI18N
+        jButton2.setText("Login");
+        jButton2.setBorder(null);
+        jButton2.setHorizontalAlignment(javax.swing.SwingConstants.LEFT);
+        jButton2.setIconTextGap(5);
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
+            }
+        });
+        add(jButton2, new org.netbeans.lib.awtextra.AbsoluteConstraints(150, 120, -1, -1));
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_jButton1ActionPerformed
+    private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
+       
+    }//GEN-LAST:event_jButton3ActionPerformed
+
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+ String adminUsername = "admin";
+        String adminPassword = "admin"; 
+
+        String enteredUsername = username.getText();
+        char[] enteredPassword = password.getPassword();
+        if (enteredUsername.equals(adminUsername) && Arrays.equals(enteredPassword, adminPassword.toCharArray())) {
+           Preferences prefs = Preferences.userNodeForPackage(Login.class);
+            prefs.putBoolean("isLoggedIn", true);
+            JOptionPane.showMessageDialog(this, "Admin login successful!");
+
+            main m = new main();
+            m.MainID.setText("Admin"); 
+            m.setExtendedState(JFrame.MAXIMIZED_BOTH); 
+            m.setVisible(true);
+            setVisible(false);
+        } else {
+
+            main m = new main();
+            userController controller = new userController();
+            ModelUser login = new ModelUser();
+
+
+            if (enteredUsername != null && enteredPassword != null) {
+                login.setUserName(enteredUsername);
+                login.setPassWord(enteredPassword);
+
+                ModelUser result = controller.Login(login);
+                if (result != null) {
+                    Preferences prefs = Preferences.userNodeForPackage(Login.class);
+                    prefs.putBoolean("isLoggedIn", true);
+                    
+                    m.MainID.setText(result.getUserId());
+                    m.setExtendedState(JFrame.MAXIMIZED_BOTH); 
+                    m.setVisible(true);
+                    setVisible(false);
+                } else {
+                    JOptionPane.showMessageDialog(this, "Incorrect Username or Password, please Try again!");
+                }
+            } else {
+                JOptionPane.showMessageDialog(this, "Username or Password cannot be empty!");
+            }
+        }        
+    }//GEN-LAST:event_jButton2ActionPerformed
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton jButton1;
-    private javax.swing.JLabel label;
+    private javax.swing.JButton jButton2;
+    private javax.swing.JButton jButton3;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
     public javax.swing.JLabel loging;
+    private Sygma.Components.PanelBar p1;
+    private Sygma.Components.PanelBar p2;
+    private Sygma.Components.PanelBar panelBar3;
+    private javax.swing.JPasswordField password;
+    private javax.swing.JTextField username;
     // End of variables declaration//GEN-END:variables
 }
